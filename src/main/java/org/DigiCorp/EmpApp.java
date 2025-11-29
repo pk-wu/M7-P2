8783bde2ee768a349fed9156f6f0697cb603bb44
pk-wu <pattanapong1997k@gmail.com>
2025-11-29 17:21:08 +0800

    updated to use ObjectMapperContextResolver

--- src/main/java/org/DigiCorp/EmpApp.java
+++ src/main/java/org/DigiCorp/EmpApp.java
@@ -3,6 +3,7 @@ package org.DigiCorp;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 import org.DigiCorp.resource.EmployeeResource;
+import org.DigiCorp.util.ObjectMapperContextResolver;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -13,6 +14,7 @@ public class EmpApp extends Application {
     public Set<Class<?>> getClasses() {
         Set<Class<?>> s = new HashSet<Class<?>>();
         s.add(EmployeeResource.class);
+        s.add(ObjectMapperContextResolver.class);
         return s;
     }
 }
